7720231062d0099c6cd8bc137be5dd42bd7029da
alinamiryuk <alinamiryuk@mail.ru>
2021-01-08 14:43:23 +0300

    auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import './App.css'
+import { Navbar } from './components/Navbar/Navbar'
+import { useSelector } from 'react-redux'
+import { useRoutes } from './routes'
 
 function App() {
+  const checkAuth = useSelector((state) => state.auth.success)
+  const routes = useRoutes(checkAuth)
+console.log(checkAuth);
   return (
-    <>check</>
+    <>
+      <Navbar />
+      {routes}
+    </>
   )
 }
 
---
+++ frontend/src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { Login } from './components/Login/Login'
+import { Menu } from './components/Menu/Menu'
+import { Signup } from './components/Signup/Signup'
+
+export const useRoutes = (auth) => {
+  if (auth) {
+return (
+    <Switch>
+      {/* <Route path="/order" component={} /> */}
+      <Route path="/" component={Menu} />
+      <Redirect to="/" />
+    </Switch>
+  )
+  } else {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/" component={Menu} />
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
+  
+}
